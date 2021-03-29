b466f9e3b4efdc35049e513f0e9df36760bf05ad
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-03-29 16:40:09 +0550

    feat(main.rs): add call to command line

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
-    println!("Hello World!");
-}
+use cli::get_program;
+use printr::Arguments;
+use std::env;
+mod cli;
+mod lib;
 
+fn main() {
+    get_program();
+    let args: Vec<String> = env::args().collect();
+    let args = Arguments::new(args);
+    // println!("{:#?}", args);
+}
